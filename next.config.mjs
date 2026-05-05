92558b85a551d5063fade41804e2eaefdfdc7d6e
Mhghazy <mahmoudhghazy@hotmail.com>
2026-05-05 17:41:57 +0300

    docs: add local development guide and fix pages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // basePath: '/dar-el-meamar-next', // DISABLING FOR LOCAL DEBUGGING
+  basePath: '/dar-elmeamar-next-v2',
   images: {
     unoptimized: true,
   },
